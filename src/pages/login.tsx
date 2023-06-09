b5c32d51a16c41dc8ee4f5331fecaea3d945bf4e
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-06-09 16:46:26 +0530

    Addition of Login🔒
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,18 @@
-"use client";
 import { browserClient } from "@cms/components/supabaseClient";
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function AuthForm() {
-  console.log(process.env);
   let supabase = browserClient;
   return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
-      showLinks={false}
-      providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
-    />
+    <section className="flex flex-auto items-center justify-center dark:bg-zinc-800 h-screen">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        showLinks={false}
+        providers={["google"]}
+        redirectTo="http://localhost:3000/"
+      />
+    </section>
   );
 }
