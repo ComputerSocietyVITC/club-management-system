b80ca0900937f79c4f999bdbf483873ae7aaa8b8
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-06-09 13:04:48 +0530

    Addition of new ignores❌
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "@cms/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "@cms/apollo/client";
+import "@cms/styles/globals.css";
+import { type AppType } from "next/dist/shared/lib/utils";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   // @ts-ignore
