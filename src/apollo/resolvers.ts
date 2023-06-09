a06f94aa78fbc03378b975a6c38f599eb6ace480
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-06-09 13:05:36 +0530

    Addition of resolvers (#8)
    * Addition of new features
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Addition of new resolvers and schema🚀
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Addition of new type definitions for resolvers🪝
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Added the resolvers and a attribute in schema
    
    * Addition of new schema, typedefintions and resolvers⭐
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Addition of Upstream merge🛠️
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Addition of new ignores❌
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    ---------
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    Co-authored-by: Shakti Nayak <nshakti.10@gmail.com> and @ashwin-p

--- src/apollo/resolvers.ts
+++ src/apollo/resolvers.ts
@@ -1,7 +1,202 @@
+import { prisma } from "@cms/server/db";
+import { Context } from "@cms/server/prisma";
+import {
+  Attends,
+  Events,
+  Projects,
+  Tasks,
+  Users,
+  WorksOn,
+  Clubs,
+} from "@prisma/client";
+
 export const resolvers = {
-    Query: {
-      viewer() {
-        return "Hello World!"
-      },
+  Query: {
+    helloWorld: () => "Hello World!",
+    getAllUsers: () => {
+      return prisma.users.findMany({});
     },
-  }
+    getAllProjects: () => {
+      return prisma.projects.findMany({});
+    },
+    getAllEvents: () => {
+      return prisma.events.findMany({});
+    },
+    getAllTasks: () => {
+      return prisma.tasks.findMany({});
+    },
+    assignedTasks: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.findUnique({
+        where: { id: _args.id },
+      }).tasks;
+    },
+    attendedEvents: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.findUnique({
+        where: { id: _args.id },
+      }).Attends;
+    },
+    isAdmin: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.findUnique({
+        where: { id: _args.id },
+        select: { role: true },
+      });
+    },
+    isActiveProject: (_parent: any, _args: Projects, context: Context) => {
+      return prisma.projects.findUnique({
+        where: { id: _args.id },
+        select: { status: true },
+      });
+    },
+    openProjects: () => {
+      return prisma.projects.findMany({ where: { status: true } });
+    },
+    openTasks: () => {
+      return prisma.tasks.findMany({ where: { status: true } });
+    },
+    eventAttendees: (_parent: any, _args: Events, context: Context) => {
+      return prisma.events.findUnique({
+        where: { id: _args.id },
+      }).Attends;
+    },
+    members: (_parent: any, _args: Clubs, context: Context) => {
+      return prisma.clubs.findUnique({ where: { id: _args.id } }).Members;
+    },
+  },
+  Mutation: {
+    createUser: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.create({
+        data: {
+          regNo: _args.regNo,
+          userName: _args.userName,
+          contactNo: _args.contactNo,
+          role: _args.role,
+        },
+      });
+    },
+    createTask: (_parent: any, _args: Tasks, context: Context) => {
+      return prisma.tasks.create({
+        data: {
+          description: _args.description,
+          status: true,
+          userId: _args.userId,
+          projectID: _args.projectID,
+        },
+      });
+    },
+    createProject: (_parent: any, _args: Projects, _context: Context) => {
+      return prisma.projects.create({
+        data: {
+          title: _args.title,
+          description: _args.description,
+          status: true,
+          link: _args.link,
+          contactNo: _args.contactNo,
+          role: _args.role,
+          clubID: _args.clubID,
+        },
+      });
+    },
+    createEvent: (_parent: any, _args: Events, context: Context) => {
+      return prisma.events.create({
+        data: {
+          name: _args.name,
+          startsAt: new Date(_args.startsAt).toISOString(),
+          venue: _args.venue,
+        },
+      });
+    },
+    assignTask: (_parent: any, _args: WorksOn, context: Context) => {
+      return prisma.worksOn.create({
+        data: {
+          user: { connect: { id: _args.userID } },
+          task: { connect: { id: _args.taskID } },
+        },
+      });
+    },
+    attendEvent: (_parent: any, _args: Attends, context: Context) => {
+      return prisma.attends.create({
+        data: {
+          user: { connect: { id: _args.userID } },
+          event: { connect: { id: _args.eventID } },
+        },
+      });
+    },
+    updateTask: (_parent: any, _args: any, context: Context) => {
+      return prisma.tasks.update({
+        where: { id: _args.taskID },
+        data: {
+          description: _args.description,
+          status: _args.status,
+        },
+      });
+    },
+    updateProjectLink: (_parent: any, _args: any, context: Context) => {
+      return prisma.projects.update({
+        where: { id: _args.projectID },
+        data: {
+          link: _args.link,
+        },
+      });
+    },
+    updateProjectDescription: (
+      _parent: any,
+      _args: Projects & { projectId: string },
+      context: Context
+    ) => {
+      return prisma.projects.update({
+        where: { id: _args.projectId },
+        data: {
+          description: _args.description,
+        },
+      });
+    },
+    updateProjectStatus: (
+      _parent: any,
+      _args: Projects & { projectId: string },
+      context: Context
+    ) => {
+      return prisma.projects.update({
+        where: { id: _args.projectId },
+        data: {
+          status: _args.status,
+        },
+      });
+    },
+    updateUserRole: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.update({
+        where: { id: _args.id },
+        data: {
+          role: _args.role,
+        },
+      });
+    },
+    updateUserContactNo: (_parent: any, _args: Users, context: Context) => {
+      return prisma.users.update({
+        where: { id: _args.id },
+        data: {
+          contactNo: _args.contactNo,
+        },
+      });
+    },
+    removeProject: (_parent: any, _args: Projects, context: Context) => {
+      return prisma.projects.delete({
+        where: { id: _args.id },
+      });
+    },
+    removeTask: (_parent: any, _args: Tasks, context: Context) => {
+      return prisma.tasks.delete({
+        where: { id: _args.id },
+      });
+    },
+    removeEvent: (_parent: any, _args: Events, context: Context) => {
+      return prisma.events.delete({
+        where: { id: _args.id },
+      });
+    },
+    removeAttendee: (_parent: any, _args: Attends, context: Context) => {
+      return prisma.attends.delete({
+        where: {},
+      });
+    },
+  },
+};
--- src/apollo/typedefs.ts
+++ src/apollo/typedefs.ts
@@ -1,13 +1,144 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const typeDefs = gql`
   type User {
-    id: ID!
-    name: String!
-    status: String!
+    id: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    regNo: String!
+    userName: String!
+    contactNo: Int!
+    role: String
+    assignedTasks: [Task!]!
   }
 
-  type Query {
-    viewer: String
+  type Clubs {
+    id: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    name: String!
+    facultyCoordinator: String
+    email: String!
+    Projects: [Project]
+    Members: [Members]
   }
-`
+
+  type Task {
+    id: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    description: String!
+    status: Boolean!
+    contactNo: Int!
+    userId: String!
+    assignedTo: Project!
+    projectId: String
+  }
+
+  input TaskInput {
+    description: String!
+    status: Boolean!
+    createdBy: String!
+    assignedTo: String!
+  }
+
+  type Project {
+    id: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    description: String
+    link: String
+    clubID: String
+    title: String!
+    status: String
+    contactNo: Int!
+    role: String
+  }
+
+  type Event {
+    id: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    name: String!
+    startsAt: DateTime!
+    venue: String!
+    attendees: [User!]
+  }
+
+  type Members {
+    id: Int!
+    userId: String!
+    clubId: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type WorkOn {
+    id: Int!
+    user: User!
+    task: Task!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type Attend {
+    id: Int!
+    user: User!
+    event: Event!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  scalar DateTime
+
+  type Query {
+    helloWorld: String!
+    getAllUsers: [User!]
+    getAllProjects: [Project!]
+    getAllTasks: [Task!]
+    getAllEvents: [Event!]
+    assignedTasks(id: String!): [Task]
+    attendedEvents(id: String!): [Event]
+    isAdmin(id: String!): User
+    isActiveProject(id: String!): Project
+    openProjects: [Project]
+    openTasks: [Task]
+    eventAttendees(id: Event): [Attend]
+    memebrs(id: Clubs): [Members]
+  }
+  type Mutation {
+    createUser(
+      regNo: String!
+      userName: String!
+      contactNo: Int!
+      role: String
+    ): User!
+    createTask(
+      description: String!
+      contactNo: Int!
+      userID: Int!
+      projectId: Int!
+      userid: String!
+      projectid: String!
+    ): Task!
+    createProject(
+      title: String!
+      status: String
+      contactNo: Int!
+      role: String
+    ): Project!
+    createEvent(name: String!, startsAt: DateTime!, venue: String!): Event!
+    assignTask(userid: String!, taskid: String!): WorkOn!
+    attendEvent(userid: String!, eventid: String!): Attend!
+    updateTask(params: TaskInput): Task!
+    updateProjectLink(projectid: String!, link: String!): Project!
+    updateProjectDescription(projectid: String!, description: String): Project!
+    updateProjectStatus(projectid: String!, status: String): Project!
+    updateUserRole(userid: String!, role: String): User!
+    updateUserContactNo(userid: String!, contactNo: Int): User!
+    removeProject(projectid: String!): Project
+    removeAttendee(attendeeid: String!): Attend!
+    removeEvent(eventid: String!): Event
+    removeTask(taskId: String): Task
+  }
+`;
