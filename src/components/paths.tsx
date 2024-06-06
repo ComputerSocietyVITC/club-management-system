b7c0eeb88dcc941f81a8ae99ced48794754b783d
sam-shervin <samshervin.sebastian@gmail.com>
2024-06-06 14:27:57 +0530

    major ui changes

---
+++ src/components/paths.tsx
@@ -0,0 +1,20 @@
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Profile",
+    link: "/profile",
+  },
+  {
+    name: "Documentation",
+    link: "/documentation",
+  },
+  {
+    name: "Leaderboard",
+    link: "/leaderboard",
+  },
+];
+
+export default navItems;
